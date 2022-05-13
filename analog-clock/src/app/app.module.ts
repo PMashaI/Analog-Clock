0652f16f9897c22447fcc3aac40b1bd9ff408030
Mariya <mariya.pchelnikova.i@gmail.com>
2022-05-13 14:26:26 +0400

    main: final refactoring

--- analog-clock/src/app/app.module.ts
+++ analog-clock/src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AnalogClockComponent } from './analog-clock/analog-clock.component';
 import { HandArrowComponent } from './analog-clock/hand-arrow/hand-arrow.component';
-
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
